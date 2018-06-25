37aff70d71feb83dd644ef0e84e65447ba1530fe
Hugo Matalonga <hmatalonga@gmail.com>
2018-06-25 17:08:19 +0100

    fix: google analytics tracking id

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,22 +7,36 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A collaborative approach to power consumption analysis of Android devices' },
-      { keywords: 'GreenHub,Android,Battery Saver,Battery Monitor,Collaborative,Power consumption' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'A collaborative approach to power consumption analysis of Android devices'
+      },
+      {
+        keywords:
+          'GreenHub,Android,Battery Saver,Battery Monitor,Collaborative,Power consumption'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato|Montserrat|Muli' }
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Lato|Montserrat|Muli'
+      }
     ]
   },
   /**
    * Modules
-  */
+   */
   modules: [
     // Simple usage
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-49971884-5'
-    }]
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: 'UA-49971884-4'
+      }
+    ]
   ],
   /*
   ** Global CSS
@@ -43,15 +57,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
