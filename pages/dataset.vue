1de292b97ecf388237466ad531226b539c04951c
Hugo Matalonga <hmatalonga@gmail.com>
2026-01-31 11:51:49 +0000

    chore: update dataset links

--- pages/dataset.vue
+++ pages/dataset.vue
@@ -7,9 +7,9 @@
         <p>In this page is available the latest version of the GreenHub dataset. It was generated on <strong>August 2020</strong>.</p>
         <p>The dataset contains the raw data formatted in csv text files. It is organized as follows:</p>
         <ul>
-          <li><a href="https://farmer.greenhubproject.org/storage/dataset/dataset-devices.7z">Devices</a></li>
-          <li><a href="https://farmer.greenhubproject.org/storage/dataset/dataset-samples.7z">Samples</a></li>
-          <li><a href="https://farmer.greenhubproject.org/storage/dataset/dataset-app_processes.7z">App processes</a></li>
+          <li><a href="https://www.dropbox.com/scl/fi/m7cnfyewsuu1nk83ucl07/dataset-devices.7z?rlkey=rni9vlzg3qvi8richcb48rsuw&st=3v5r0ouc&dl=0">Devices</a></li>
+          <li><a href="https://www.dropbox.com/scl/fi/8bgd1o881xino99908djp/dataset-samples.7z?rlkey=b34h6n86md6vme1845ape3zw8&st=6eq5emn5&dl=0">Samples</a></li>
+          <li><a href="#">App processes</a> (Not Available)</li>
         </ul>
         <p>Each dataset file is paginated, therefore each part is split in small digestable files. All download files are compressed in 7z format for storage convenience.</p>
         <p>For documentation about the schema and contents of the dataset, please refer to our paper <a href="http://greenlab.di.uminho.pt/wp-content/uploads/2019/04/GreenHubFarmerFinal.pdf">GreenHub Farmer: Real-world data for Android Energy Mining</a>.</p>
